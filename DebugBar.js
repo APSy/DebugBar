cce1404c8761960005678e5110a67aab2b8ce101
Kenan Regez <dapurehate@gmail.com>
2014-06-24 20:50:28 +0200

    Beta version finished and polished a bit

--- DebugBar.js
+++ DebugBar.js
@@ -2,7 +2,6 @@
  * Created by APSy on 17/06/14.
  */
 
-
 var DebugBar = function() {
 
     this.defaultConfig = {
@@ -114,7 +113,7 @@ var DebugBar = function() {
         var style = document.createElement("style");
         var linkCss = document.createElement("link");
         linkCss.setAttribute('rel', 'stylesheet');
-        linkCss.setAttribute('href', 'css/debugBar.css');
+        linkCss.setAttribute('href', 'DebugBar/css/debugBar.css');
         var css = document.createTextNode('#debugBar { color:'+this.defaultConfig.color+'; font-size:'+this.defaultConfig.fontsize+'px; }');
         style.appendChild(css);
         document.body.appendChild(linkCss);
